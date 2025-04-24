53bf21f7d7fd1bc5f37fbc34613821a887c8f63f
mac <mac@macs-MacBook-Air.local>
2025-04-24 17:06:41 +0530

    adding changes in index.js and package.json

--- api/index.js
+++ api/index.js
@@ -5,34 +5,47 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
 import listingRouter from './routes/listing.route.js'
+import path from 'path'
+
+
 
 dotenv.config()
 
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to mongodb')
-}).catch((err)=>console.log('Error',err))
+}).catch((err) => console.log('Error', err))
 
-const app=express()
+const app = express()
+
+const _dirname = path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
 
-app.listen(3000,()=>{
-    console.log('server is running on 3000')
-})
-
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
-app.use('/api/listing',listingRouter)
 
 
-app.use((err,req,res,next)=>{
-    const statusCode= err.statusCode || 500
-    const message= err.message ||'Internal Server Error'
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/listing', listingRouter)
+
+app.use(express.static(path.join(_dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, 'client/dist', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
 
     return res.status(statusCode).json({
-        success:false,
+        success: false,
         message,
         statusCode
     })
+})
+
+app.listen(3000, () => {
+    console.log('server is running on 3000')
 })
